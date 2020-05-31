3fdad9584312aad2957c9dd38fe13c7b98f25f57
Aliaksandr <klut2018a@gmail.com>
2020-05-31 08:05:57 +0300

    Thread: detach() and join().

--- Development of multithreaded applications in OS Window/main.cpp
+++ Development of multithreaded applications in OS Window/main.cpp
@@ -4,15 +4,43 @@
 
 using namespace std;
 
+void Work1()
+{
+	for (size_t i = 0; i < 10; i++)
+	{
+		cout << "ID потока = " << this_thread::get_id() << "\tWork1" << endl;
+		this_thread::sleep_for(chrono::milliseconds(2000));
+	}
+}
+
+
+void Work2()
+{
+	for (size_t i = 0; i < 10; i++)
+	{
+		cout << "ID потока = " << this_thread::get_id() << "\tWork2" << endl;
+		this_thread::sleep_for(chrono::milliseconds(1000));
+	}
+}
+
 int main()
 {
 	setlocale(LC_ALL,"ru");
 
+	thread th1(Work1);
+	th1.detach();
+
+	thread th2(Work2);
+	th2.join();
+
 	cout << "Star main" << endl;
+	for (size_t i = 0; i < 10; i++)
+	{
+		cout << "ID потока = " << this_thread::get_id() << "\tmain" << endl;
+		this_thread::sleep_for(chrono::milliseconds(500));
+	}
+	cout << "End main" << endl;
 
-	this_thread::sleep_for(chrono::milliseconds(5000));
-
-	cout << this_thread::get_id() << endl;
-
+	
 	return 0;
 }
