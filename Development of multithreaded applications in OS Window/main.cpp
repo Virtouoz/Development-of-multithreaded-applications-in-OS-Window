60397e8408bd7707ef54a91b05a04c611df1bfc6
Aliaksandr <klut2018a@gmail.com>
2020-06-03 18:54:11 +0300

    Thread Priority (NORMAL and CRITICAL)

--- Development of multithreaded applications in OS Window/main.cpp
+++ Development of multithreaded applications in OS Window/main.cpp
@@ -2,19 +2,59 @@
 #include <thread>
 #include <chrono>
 #include <string>
+#include <Windows.h>
 
 using namespace std;
 
 void Work1(string str)
 {
-	cout << "ID потока = " << this_thread::get_id() << "\tWork1" << endl;
+	cout << "ID потока = " << this_thread::get_id() << endl;
 	int a;
 	for (int i = 0; i < str.length(); i++)
 	{
 		str[i] = str[i] - 129;
 		cout << str[i];
 	}
-	cout << endl;
+	cout << endl << endl;
+}
+
+void lab1(string str)
+{
+	auto start = chrono::system_clock::now();
+
+	thread th1(Work1, str);
+	th1.join();
+
+	auto end = chrono::system_clock::now();
+
+	chrono::duration<double> time = end - start;
+	cout << "Time = " << time.count() << endl;
+}
+
+void lab2(string str)
+{
+	auto start1 = chrono::system_clock::now();
+
+	thread th1(Work1, str);
+	SetThreadPriority(th1.native_handle(), THREAD_PRIORITY_NORMAL);
+	th1.join();
+
+	auto end1 = chrono::system_clock::now();
+
+	chrono::duration<double> time1 = end1 - start1;
+	cout << "Time Normal = " << time1.count() << endl << endl;
+
+
+	auto start2 = chrono::system_clock::now();
+
+	thread th2(Work1, str);
+	SetThreadPriority(th2.native_handle(), THREAD_PRIORITY_TIME_CRITICAL);
+	th2.join();
+
+	auto end2 = chrono::system_clock::now();
+
+	chrono::duration<double> time2 = end2 - start2;
+	cout << "Time Critical = " << time2.count() << endl << endl;
 }
 
 int main()
@@ -26,8 +66,9 @@ int main()
 	cin >> str;
 	cout << endl;
 
-	thread th1(Work1,str);
-	th1.join();
+	lab1(str);
+
+	lab2(str);
 
 	return 0;
 }
